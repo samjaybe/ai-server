616a9e531559aa63f72e5c444391913b064cdec4
samjaybe <133429185+samjaybe@users.noreply.github.com>
2025-04-07 00:11:09 +0200

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import requests
 import ollama
+import os
 
 app = Flask(__name__)
 
@@ -43,4 +44,5 @@ def analysera():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
